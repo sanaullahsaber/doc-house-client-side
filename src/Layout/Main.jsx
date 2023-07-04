f07660dc0b416c5458ee69c691af57d34644be90
sanaullahsabere <you@example.com>
2023-07-04 12:31:15 +0600

    navbar done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Footer from '../components/Shared/Footer/Footer';
+import NavBar from '../components/Shared/Navbar/Navbar';
 
 const Main = () => {
   return (
     <div>
-      
+      <NavBar></NavBar>
+      <Footer></Footer>
     </div>
   );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
 
 const router = createBrowserRouter([
   {
@@ -7,7 +8,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element
+       
       }
     ]
   },
